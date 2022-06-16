6661962903088b181353b2e67c257d5faae6788c
mdv <malykh.d.vla@sbrebank.ru>
2022-06-16 23:07:38 +0300

    GraphQL cursor and generated methods

---
+++ src/api/graphql/service/cursor/cursor.go
@@ -0,0 +1,30 @@
+package cursor
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/gob"
+	"fmt"
+	"time"
+)
+
+func Marshal[V string | time.Time | uint | int64](value V) string {
+	var buf = new(bytes.Buffer)
+	if err := gob.NewEncoder(buf).Encode(value); err != nil {
+		//@TODO log. Impossible situation, because we have full control on type for value
+	}
+	return base64.StdEncoding.EncodeToString(buf.Bytes())
+}
+
+// Unmarshal decoder value string to cursor
+func Unmarshal(value string, v any) error {
+	decoded, err := base64.StdEncoding.DecodeString(value)
+	if err != nil {
+		return fmt.Errorf(`error to unmarshal cursor %q %w`, value, err)
+	}
+
+	if err := gob.NewDecoder(bytes.NewBuffer(decoded)).Decode(v); err != nil {
+		return fmt.Errorf(`error to decode cursor %q %w`, value, err)
+	}
+	return nil
+}
---
+++ src/api/graphql/service/cursor/cursor_test.go
@@ -0,0 +1,82 @@
+package cursor
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestEncodeDecode(t *testing.T) {
+
+	type args struct {
+		value   any
+		entropy any
+	}
+
+	type testCase struct {
+		name   string
+		encode func() string
+		need   any
+		err    error
+	}
+	var timeExample = time.Now()
+	tests := []testCase{
+		{
+			name: `Correct simple encoding and decoding string value`,
+			encode: func() string {
+				return Marshal("hello")
+			},
+			need: "hello",
+		},
+		{
+			name: `Correct simple encoding and decoding long string value`,
+			encode: func() string {
+				return Marshal("OI*H(PG&fo6dc8yvp97g8P&F6oc8yvp9g87fo6DCYuviu9gp7f86odctuyviu9gp7f8o6cyuviu97gf8o6cuyvig9p7f8o6cyuvig7f8o6cyuvg97f8o6cyvg7f8o6cvy7g8f")
+			},
+			need: "OI*H(PG&fo6dc8yvp97g8P&F6oc8yvp9g87fo6DCYuviu9gp7f86odctuyviu9gp7f8o6cyuviu97gf8o6cuyvig9p7f8o6cyuvig7f8o6cyuvg97f8o6cyvg7f8o6cvy7g8f",
+		},
+		{
+			name: `Correct simple encoding and decoding time value`,
+			encode: func() string {
+				return Marshal(timeExample)
+			},
+			need: timeExample,
+		},
+		{
+			name: `Correct simple encoding and decoding int64 value`,
+			encode: func() string {
+				return Marshal(int64(433245362531))
+			},
+			need: int64(433245362531),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var encoded = tt.encode()
+			assert.NotEmpty(t, encoded)
+
+			switch tt.need.(type) {
+			case string:
+				var v string
+				assert.NoError(t, Unmarshal(encoded, &v))
+				assert.Equal(t, tt.need, v)
+				break
+			case int64:
+				var v int64
+				assert.NoError(t, Unmarshal(encoded, &v))
+				assert.Equal(t, tt.need, v)
+				break
+			case time.Time:
+				var v time.Time
+				assert.NoError(t, Unmarshal(encoded, &v))
+				assert.True(t, tt.need.(time.Time).Equal(v))
+				//assert.Equal(t, tt.need.(time.Time).String(), v.String())
+				break
+			default:
+				panic("Unknown type of tt.need")
+				break
+			}
+
+		})
+	}
+}
